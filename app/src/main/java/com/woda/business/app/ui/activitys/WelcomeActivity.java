24b41874dd1f54e1e302450a8a547474fa13d5b1
ArthurTcd <372539829@qq.com>
2018-05-15 14:26:26 +0800

    补充工具类
    Signed-off-by: ArthurTcd <372539829@qq.com>

--- app/src/main/java/com/woda/business/app/ui/activitys/WelcomeActivity.java
+++ app/src/main/java/com/woda/business/app/ui/activitys/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package com.woda.business.app.ui.activitys;
 
+import android.content.Context;
+
 import com.arthur.tu.base.R;
 import com.arthur.tu.base.base.BaseActivity;
+import com.arthur.tu.base.base.BaseView;
 
 public class WelcomeActivity extends BaseActivity {
     @Override
--- baseLibrary/src/main/java/com/arthur/tu/base/base/BaseView.java
+++ baseLibrary/src/main/java/com/arthur/tu/base/base/BaseView.java
@@ -1,5 +1,9 @@
 package com.arthur.tu.base.base;
 
+import android.content.Context;
+
 public interface BaseView {
     void showError(String msg);
+
+    Context getContext();
 }
